931d955ffae4f5cc3d355da97ce108d0ea96b900
abrosua <abrosua@gmail.com>
2020-03-15 12:23:16 +0700

    experimenting with quiver plot using thresholding and flow statistic.

--- postpro.py
+++ postpro.py
@@ -1,27 +1,88 @@
 import sys
 import os
+from typing import Optional
 
-import torch
+from PIL import Image
+import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 
 pivdir = "/home/faber/thesis/thesis_faber"
 sys.path.append(pivdir)
-from src.utils_plot import read_flow, quiver_plot, motion_to_color
+from src.utils_plot import read_flow, motion_to_color
 
 
 def singleplot(floname, imdir):
-	# Generate image name from the flo file
-	basename = str(os.path.basename(floname).rsplit('_', 1)[0])
-	subdir = os.path.basename(os.path.dirname(floname))
-	imname = os.path.join(imdir, subdir, basename + ".png")
+    # Generate image name from the flo file
+    basename = str(os.path.basename(floname).rsplit('_', 1)[0])
+    subdir = os.path.basename(os.path.dirname(floname))
+    imname = os.path.join(imdir, subdir, basename + ".tif")
 
-	plt.imshow(imname)
-	out_flow = read_flow(floname)
-	quiver_plot(out_flow, show=True)
+    # plotting image
+    im = Image.open(imname).convert('RGB')
+    plt.imshow(im)
+    out_flow = read_flow(floname)
+    quiver_plot(out_flow, thres=0.1, show=True)
+
+
+def quiver_plot(flow: np.ndarray, filename: Optional[str] = None, thres: Optional[float] = None,
+                show: bool = False) -> None:
+    flow = _quiver_clean(flow, threshold=thres) if thres and thres > 1 else flow
+    u = flow[:, :, 0]
+    v = flow[:, :, 1]
+
+    # Setting up the quiver plot
+    h, w = u.shape
+    x = np.arange(0, w) + 0.5
+    y = np.arange(0, h)[::-1] + 0.5
+    xp, yp = np.meshgrid(x, y)
+
+    # ploting the result
+    plt.quiver(xp, yp, u, v)
+    plt.axis('equal')
+    if show:
+        plt.show()
+
+    if filename is not None:
+        assert type(filename) is str, "File is not str (%r)" % str(filename)
+        assert filename[-4:] == '.png', "File extension is not an image format (%r)" % filename[-4:]
+        plt.savefig(filename)
+
+    plt.clf()
+
+
+def _quiver_clean(flow: np.array, threshold: float = 0.05) -> np.array:
+    flowval = np.linalg.norm(flow, axis=2)
+    flow[flowval <= threshold] = np.nan  # avoid plotting vector
+
+    return flow
+
+
+def _checkstat(data):
+    sns.distplot(data, hist=True, kde=True,
+                 bins=int(180 / 5), color='darkblue',
+                 hist_kws={'edgecolor': 'black'},
+                 kde_kws={'linewidth': 4})
+    plt.show()
 
 
 if __name__ == '__main__':
-	imdir = "./frames"
-	floname = "./results/Test 03 L3 NAOCL 22000 fpstif_100/Test 03 L3 NAOCL 22000 fpstif_0000_out.flo"
-	singleplot(floname, imdir)
+    # Image source
+    imdir = "./frames"
+
+    # Flow source
+    inputdir = 'Test 03 L3 NAOCL 22000 fpstif'
+    flodirs = [os.path.join('./results', netname) for netname in ['Hui-LiteFlowNet', 'PIV-LiteFlowNet-en']]
+    flosubdirs = [inputdir + f"_{i}" if i else inputdir for i in [None, 5, 10, 100]]
+
+    floname = os.path.join(flodirs[0], flosubdirs[0], 'Test 03 L3 NAOCL 22000 fpstif_0000_out.flo')
+
+    # Plotting
+    out_flow = read_flow(floname)
+    flowstat = np.linalg.norm(out_flow, axis=2).flatten()
+    # _checkstat(flowstat)
+
+    # quiver_plot(out_flow, thres=0.5, show=True)
+    singleplot(floname, imdir)
+
+    print('DONE!')
