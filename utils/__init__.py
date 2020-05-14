6df2a6fb3c9e0b395d22ac7a89f73b8c955445ed
Faber Silitonga <abrosua@gmail.com>
2020-05-14 12:05:03 +0700

    adding the liteflownet modules to the utils init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,15 @@
+import os
+import sys
+
 # init for post-processing
 from .plot import singleplot, quiver_plot, checkstat
 from .postpro import get_label, use_flowviz, velo_mean
+
+# Manage the working directory
+maindir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(maindir)
+
+# Importing from LiteFlowNet
+pivdir = os.path.join(os.path.dirname(maindir), "thesis_faber")
+sys.path.append(pivdir)
+from src.utils_plot import read_flow, read_flow_collection
