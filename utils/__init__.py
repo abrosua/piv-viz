edaab271b86d5b44969c496eafe9155304d8f7f6
Faber Silitonga <abrosua@gmail.com>
2020-10-14 16:28:00 +0700

    adding/modifying some functions in init

--- utils/__init__.py
+++ utils/__init__.py
@@ -14,4 +14,5 @@ os.chdir(maindir)
 pivdir = os.path.join(os.path.dirname(maindir), "thesis_faber")
 sys.path.append(pivdir)
 from src.utils_plot import read_flow, read_flow_collection, array_cropper
+from src.postpro import calc_vorticity, de_vort
 from inference import Inference, flowname_modifier, write_flow, piv_liteflownet, hui_liteflownet
