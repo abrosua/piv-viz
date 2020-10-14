30ca44ebb24a763097678a814daeda97aebd2bc6
Faber Silitonga <abrosua@gmail.com>
2020-10-14 16:39:20 +0700

    added multiplotting feature, also included vorticity and shear stress for the color plot mode

--- utils/plot.py
+++ utils/plot.py
@@ -1,6 +1,7 @@
 import os
 import imageio
 import math
+from glob import  glob
 from typing import Optional, List, Union, Tuple
 
 import numpy as np
@@ -13,6 +14,7 @@ from matplotlib import colors
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from flowviz import colorflow, animate
 from PIL import Image
+from tqdm import tqdm
 
 import utils
 import matplotlib
@@ -22,8 +24,8 @@ matplotlib.use('TkAgg')
 class FlowViz:
     def __init__(self, labelpaths: List[str], netname: str, maxmotion: Optional[float] = None, vector_step: int = 1,
                  use_color: int = 0, use_quiver: bool = False, color_type: Optional[str] = None, key: str = "flow",
-                 crop_window: Union[int, Tuple[int, int, int, int]] = 0, velocity_factor: float = 1.0,
-                 verbose: int = 0) -> None:
+                 crop_window: Union[int, Tuple[int, int, int, int]] = 0, calib: float = 1.0, fps: int = 1,
+                 factor: Optional[float] = None, verbose: int = 0) -> None:
         """
         Flow visualization instance
         params:
@@ -37,7 +39,7 @@ class FlowViz:
         assert len(labelpaths) > 0
         assert type(use_color) == int and use_color < 3
         if color_type is not None:
-            assert color_type in ["vort", "mag"]
+            assert color_type in ["vort", "shear", "normal", "mag"]
 
         self.labelpaths = labelpaths
         self.netname = netname
@@ -52,7 +54,8 @@ class FlowViz:
         self.maxmotion = maxmotion
         self.vector_step = vector_step
         self.crop_window = crop_window
-        self.velocity_factor = velocity_factor
+        self.fps, self.calib, self.factor = fps, calib, factor
+        self.velocity_factor = calib * fps
         self.key = key
 
         # Init.
@@ -60,22 +63,36 @@ class FlowViz:
         self.ax = self.fig.add_subplot(1, 1, 1)
         self.ax.set_xticks([])  # Erasing the axis number
         self.ax.set_yticks([])
-        plt.subplots_adjust(right=0.70) if color_type == "mag" else None
+        plt.subplots_adjust(right=0.85) if color_type in ["mag", "vort", "normal", "shear"] else None
 
         self.im1, self.quiver = None, None
         self.img_dir, self.keyname = "", ""
 
         # Scalar mapping
-        self.norm = colors.Normalize(vmin=0, vmax=self.maxmotion)
-        self.scalar_map = cm.ScalarMappable(norm=self.norm, cmap=cm.hot_r)
+        if color_type in ["vort", "shear", "normal"]:
+            self.norm = colors.TwoSlopeNorm(vcenter=0.0) if self.maxmotion is None else \
+                colors.TwoSlopeNorm(vmin=-self.maxmotion, vcenter=0.0, vmax=self.maxmotion)
+            self.scalar_map = cm.ScalarMappable(norm=self.norm, cmap=cm.coolwarm)
+        else:
+            self.norm = colors.Normalize() if self.maxmotion is None else \
+                colors.Normalize(vmin=0, vmax=self.maxmotion)
+            self.scalar_map = cm.ScalarMappable(norm=self.norm, cmap=cm.hot_r)
 
-        if (use_quiver and not use_color) or color_type == "mag":
+        if (use_quiver and not use_color) or color_type in ["mag", "vort", "shear", "normal"]:
             divider = make_axes_locatable(self.ax)
             cax = divider.append_axes("right", size="5%", pad=0.05)
             clb = self.fig.colorbar(self.scalar_map, cax=cax)
 
-            unit = "pix" if velocity_factor == 1.0 else "mm/s"
+            if color_type == "vort":
+                unit = "1/ms"
+            elif color_type in ["shear", "normal"]:
+                unit = "N/m2"
+            else:
+                unit = "pix" if self.velocity_factor == 1.0 else "mm/s"
             clb.ax.set_title(unit)
+
+        if color_type is not None:
+            self.keyname += f"{color_type}-"
 
         if self.use_color == 1:
             self.keyname += "c"
@@ -85,14 +102,17 @@ class FlowViz:
         if self.use_quiver:
             self.keyname += "q"
 
-    def plot(self, file_extension: Optional[str] = None, show: bool = False, savedir: Optional[str] = None,
+    def plot(self, ext: Optional[str] = None, show: bool = False, savedir: Optional[str] = None,
              **quiver_key) -> None:
         """
         Plotting
         """
+        init_plot = True
+
         for labelpath in self.labelpaths:
             label = utils.Label(labelpath, self.netname, verbose=self.verbose)
             bname = os.path.splitext(os.path.basename(labelpath))[0]
+            self.img_dir = bname.rsplit("_", 1)[0]
 
             # Add flow field
             flow, mask = label.get_flo(self.key)
@@ -108,21 +128,76 @@ class FlowViz:
             # img = imageio.imread(label.img_path)  # Read the raw image
             img_crop = utils.array_cropper(img, self.crop_window)
 
-            self._init_frame(img_crop, **quiver_key)  # Initialize the frame
+            if init_plot:
+                self._init_frame(img_crop, **quiver_key)  # Initialize the frame
+                init_plot = False
             self._draw_frame(flow_crop, img_crop, mask_crop)  # Drawing each frame
 
             if show:
                 plt.show()
 
-            if file_extension:
+            if ext:
                 # Setting up the path name
                 plotdir = os.path.join("./results", self.netname, self.img_dir, "viz") if savedir is None else savedir
                 os.makedirs(plotdir) if not os.path.isdir(plotdir) else None
                 # Saving the plot
-                plotpath = os.path.join(plotdir, bname + f"_{self.keyname}viz.{file_extension}")
+                plotpath = os.path.join(plotdir, bname + f"_{self.keyname}viz.{ext}")
                 plt.savefig(plotpath, dpi=300, bbox_inches='tight')
 
             plt.clf()
+
+    def multiplot(self, flodir: str, ext: Optional[str] = None, show: bool = False, imgext: str = "tif",
+                  start_at: int = 0, num_images: int = -1) -> None:
+        """
+        Generating multiple image plots with a single label.
+        """
+        # Flow init.
+        assert num_images != 0
+        flonames = sorted(glob(os.path.join(flodir, "*.flo")))
+        flonames = flonames[start_at:] if num_images < 0 else flonames[start_at : num_images+start_at]
+        num_images = len(flonames) if num_images < 0 else num_images
+
+        self.img_dir = str(os.path.normpath(flodir).split(os.sep)[-2])
+        imgdir = os.path.join("./frames", self.img_dir)
+        plotdir = os.path.join(os.path.dirname(flodir), f"plot-{start_at}_{num_images}")
+        os.makedirs(plotdir) if not os.path.isdir(plotdir) else None
+
+        # Frame looping init.
+        if len(self.labelpaths) > 1:
+            raise ValueError(f"Multiple labelpaths are found! ('{self.labelpaths}')")
+        assert os.path.isfile(self.labelpaths[0])
+
+        label = utils.Label(self.labelpaths[0], self.netname, verbose=self.verbose)
+        _, mask = label.get_flo(self.key)  # Create the mask
+        mask_crop = utils.array_cropper(mask, self.crop_window)
+
+        is_init = False
+
+        for i, floname in tqdm(enumerate(flonames), desc=self.img_dir, unit="image", total=len(flonames)):
+            bname = os.path.basename(floname).rsplit("_", 1)[0]
+
+            # Importing the image
+            imagepath = os.path.join(imgdir, bname + f".{imgext}")
+            img = np.array(Image.open(imagepath).convert("RGB"))
+            img_crop = utils.array_cropper(img, self.crop_window)
+
+            # Add flow field
+            flow_crop = utils.read_flow(floname, crop_window=self.crop_window) * self.velocity_factor
+            if not is_init:
+                self._init_frame(img_crop)  # Initialize the frame
+                is_init = True
+            self._draw_frame(flow_crop, img_crop, mask_crop)  # Drawing each frame
+
+            if show:
+                plt.show()
+
+            if ext:
+                # Saving the plot
+                plotpath = os.path.join(plotdir, bname + f"_{self.keyname}viz.{ext}")
+                self.fig.savefig(plotpath, dpi=300, bbox_inches='tight')
+                # plt.savefig(plotpath, dpi=300, bbox_inches='tight')
+
+            # plt.clf()
 
     def video(self, flodir: str, ext: Optional[str] = None, imgext: str = "tif",
               start_at: int = 0, num_images: int = -1, fps: int = 1, dpi: int = 300) -> None:
@@ -185,6 +260,8 @@ class FlowViz:
                 writer = animation.writers["ffmpeg"](fps=fps, metadata=dict(artist="abrosua"), bitrate=-1)
                 flowviz_anim.save(vidpath, writer=writer, dpi=dpi)
 
+        self.ax.cla()
+
     def _capture_frame(self, idx, labelpaths: List[str], imagepaths: List[str], flows: np.array):
         """
         Iteration function for capturing vidoe frame.
@@ -237,38 +314,48 @@ class FlowViz:
         """
         Drawing each single frame.
         """
+        if self.factor is None:
+            self.factor = self.calib * 1000
+
         u, v = flow[:, :, 0], flow[:, :, 1]
 
         # Image masking
-        flow[~mask] = 0.0  # Replacing NaNs with zero to convert the value into RGB
-        mag = np.linalg.norm(flow, axis=-1)  # Calculate the flow magnitude
+        if self.color_type in ["vort", "shear", "normal"]:
+            vort, shear, normal = utils.calc_vorticity(flow, calib=self.factor)
+            if self.color_type == "vort":
+                flo_diff = vort
+            elif self.color_type == "shear":
+                flo_diff = shear
+            else:
+                flo_diff = normal
 
-        if self.color_type == "vort":  # TODO: create vorticity calculation function!
-            flo_rgb = None
+            flo_rgb = np.uint8(self.scalar_map.to_rgba(flo_diff) * 255)[:, :, :3]
+            flo_alpha = np.abs(flo_diff)  # Setting up the transparency mask
         elif self.color_type == "mag":
-            flo_rgb = np.uint8(self.scalar_map.to_rgba(mag) * 255)[:, :, :3]
+            flo_alpha = np.linalg.norm(flow, axis=-1)  # Calculate the flow magnitude
+            flo_rgb = np.uint8(self.scalar_map.to_rgba(flo_alpha) * 255)[:, :, :3]
         else:
+            flo_alpha = np.linalg.norm(flow, axis=-1)  # Calculate the flow magnitude
             flo_rgb = colorflow.motion_to_color(flow, maxmotion=self.maxmotion)
+
+        flo_alpha[~mask] = 0
 
         # Superpose the image and flow color visualization if use_color is activated
         if self.use_color == 0:
             image[mask] = 255
             self.im1.set_data(image)
-
         elif self.use_color == 1:
             flo_rgb[~mask] = 0  # Merging image and plot the result
             image[mask] = 0
             self.im1.set_data(image + flo_rgb)
-
         elif self.use_color == 2:
             flo_rgb[~mask] = 255
-            # alpha = np.uint8((mag - np.min(mag)) * 255 / (np.max(mag) - np.min(mag)))
-            alpha = np.uint8(mag * 255 / self.maxmotion)
+            alpha = np.uint8((flo_alpha - np.min(flo_alpha)) * 255 / (np.max(flo_alpha) - np.min(flo_alpha)))
+            # alpha = np.uint8(flo_alpha * 255 / self.maxmotion)
             flo_rgba = np.dstack([flo_rgb, alpha])
 
             self.im1.set_data(image)
             self.im2.set_data(flo_rgba)
-
         else:
             self.im1.set_data(image)
 
@@ -401,30 +488,41 @@ def color_map(resolution: int = 256, maxmotion: float = 1.0, show: bool = False,
     plt.clf()
 
 
-def filter_plot(csvname: str, key: List[str], avg_window: int = 1, layered: bool = False,
-                show: bool = False, filename: Optional[str] = None, title: Optional[str] = None):
+def filter_plot(csvname: str, key: List[str], avg_window: int = 1, layered: bool = False, show: bool = False,
+                filename: Optional[str] = None, title: Optional[str] = None, xlim: Optional[Tuple[float]] = None,
+                ) -> Tuple[pd.DataFrame, plt.Axes]:
     """
     Applying moving average for signal filtering, and plot it
     """
     df = pd.read_csv(csvname, index_col=0)
-    df_roll = df[key].rolling(window=avg_window, min_periods=1).mean()  # TODO: Fix the NaNs in noise filtering with moving average
+    df_roll = df[key].rolling(window=avg_window, min_periods=1).mean()
+
+    df_plot = df[['time']]
+    col_plot = []
 
     for k in key:
+        df_plot[k] = df[k]
         new_key = f"filtered_{k}" if layered else k
-        df[new_key] = df_roll[k]
+        df_plot[new_key] = df_roll[k]
 
-    if layered:
+        col_plot.append(k)
+        col_plot.append(new_key) if new_key not in col_plot else None
+
+    if layered and filename:
         fname, fext = os.path.splitext(filename)
         filename = fname + f"-layered{fext}"
 
-    df.plot()
-    plt.title(title) if title is not None else None
-    plt.xlabel("Time stamp [frame]")
-    plt.ylabel("Flow magnitude [pix]")
+    ax = df_plot.plot(x='time', y=col_plot)
+    ax.set_title(title) if title is not None else None
+    ax.set_xlabel("Time stamp [s]")
+    ax.set_ylabel("Flow magnitude [mm/s]")
+    ax.set_ylim(ymin=0)
+    ax.set_xlim(xmin=0, xmax=df_plot['time'].max())
 
     plt.show() if show else None
     plt.savefig(filename, dpi=300, bbox_inches='tight') if filename else None
-    plt.clf()
+
+    return df_plot, ax
 
 
 if __name__ == "__main__":
