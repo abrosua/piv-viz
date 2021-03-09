c8e388e7c50b30e4cf90e738c8b3e477bf548e18
Faber Silitonga <abrosua@gmail.com>
2021-03-09 19:41:49 +0700

    added gif/vid maker from series of images

--- extract.py
+++ extract.py
@@ -1,7 +1,9 @@
 import cv2
 import os
 import shutil
+from glob import glob
 
+import imageio
 from tqdm import tqdm
 from PIL import Image, ImageSequence
 
@@ -59,15 +61,52 @@ def copyfile(sourcedir, targetdir):
 
 	for i, src in tqdm(enumerate(sourcefiles), desc=sourcedir, unit="frames"):
 		srcfile = os.path.join(sourcedir, src)
-		id = src.rsplit("_", 1)[1]
+		srcname, srcext = os.path.splitext(srcfile)
+		id = srcname.rsplit("_", 1)[1]
 
-		fname = os.path.basename(targetdir) + "_" + id
+		fname = os.path.basename(targetdir) + "_" + str(id).zfill(5) + srcext
 		targetname = os.path.join(targetdir, fname)
 
 		# Copying files
 		shutil.copy2(srcfile, targetname)
 
 	tqdm.write(f"Finished copying {i+1} file(s)!")
+
+
+def im2vid(imagedir: str, savedir: str, fps: int = 1, start_at: int = 0, num_images: int = -1, vidext: str = "mp4"):
+	# Init.
+	assert os.path.isdir(imagedir)
+	assert num_images != 0
+	files = []
+
+	for ext in ('jpg', 'jpeg', 'png', 'bmp', 'tif', 'ppm'):  # Gathering image files
+		files += sorted(glob(os.path.join(imagedir, f'*.{ext}')))
+		files += sorted(glob(os.path.join(imagedir, f'*.{ext.upper()}')))
+
+	# Slicing the image file(s)
+	files_sliced = files[start_at:] if num_images < 0 else files[start_at : start_at + num_images]
+
+	# Instantiate the video files
+	if not os.path.isdir(savedir):
+		os.makedirs(savedir)
+	vidname = os.path.basename(files_sliced[0]).rsplit("_", 1)[0] + f"-{start_at}_{num_images}_{fps}vid.{vidext}"
+	vidpath = os.path.join(savedir, vidname)
+
+	# Video processing
+	print(f"Generating a video from '{imagedir}' with {len(files_sliced)} frames, starting from id = {start_at}")
+	print("Please wait ...")
+	if vidext.lower() == "gif":  # Create a gif file
+		with imageio.get_writer(vidpath, mode="I", fps=fps) as writer:
+			for filename in files_sliced:
+				image = imageio.imread(filename)
+				writer.append_data(image)
+
+	else:  # Create a video file
+		frame = cv2.imread(files_sliced[0])
+		height, width, layers = frame.shape
+		# video = cv2.VideoWriter(vidpath)
+
+	print(f"Finished generating a video at '{vidpath}'!")
 
 
 if __name__ == "__main__":
@@ -80,8 +119,16 @@ if __name__ == "__main__":
 	# gifpath = "./results/present/Test 03 L3 NAOCL 22000 fpstif-8600_90_tqvid.gif"
 	# from_gif(gifpath, keep_origin=False, ext="png")
 
+	# Generate video
+	imagedir = "./frames/Test 03 L3 NAOCL 22000 fpstif"
+	savedir = os.path.join("./results/Hui-LiteFlowNet", os.path.basename(imagedir), "videos")
+	for start_id in (2200, 7700, 11000):
+		im2vid(imagedir, savedir, vidext="gif", fps=30, start_at=start_id, num_images=1500)
+
 	# Copying file
-	targetdir = "./frames/Test 06 EDTA EA Full 22000 fps"
-	for i in reversed(range(6)):
-		sourcedir = f"./frames/Test 06 EDTA EA Full 22000 fps-0{i}"
+	names = ["dea-01-opt", "dea-02-opt",]
+	targetdirs = "../work/frames"
+	for name in names:
+		targetdir = os.path.join(targetdirs, name)
+		sourcedir = os.path.join(targetdirs, "base", name)
 		copyfile(sourcedir, targetdir)
